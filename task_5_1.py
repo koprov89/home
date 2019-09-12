8c3ea55fbd1dab9b82be4243e8ed75221fcbaab5
koprov89 <koprov89@gmail.com>
2019-09-13 00:41:54 +0300

    last

---
+++ task_5_1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from sys import argv
+
+
+london_co = {
+'r1':
+    {
+'location': '21 New Globe Walk',
+'vendor': 'Cisco',
+'model': '4451',
+'ios': '15.4',
+'ip': '10.255.0.1'
+},
+
+'r2':
+    {
+'location': '21 New Globe Walk',
+'vendor': 'Cisco',
+'model': '4451','ios': '15.4',
+'ip': '10.255.0.2'
+},
+'sw1':
+    {
+'location': '21 New Globe Walk',
+'vendor': 'Cisco',
+'model': '3850',
+'ios': '3.6.XE',
+'ip': '10.255.0.101',
+'vlans': '10,20,30',
+'routing': True
+}
+}
+devices = london_co.keys()
+dev_name = input(f"Введите имя устройства: ({devices})")
+print(london_co.keys(dev_name))
